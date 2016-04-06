be9045325a5d06ed037029ecddfe81bb5cc3d5ff
Simón Fuertes Sánchez <simonfuesan@gmail.com>
2016-04-06 22:49:24 +0100

    Añadiendo heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var port = Number(process.env.PORT || 3000);
 
 
 app.use(express.static(__dirname + '/'));
@@ -9,6 +10,6 @@ app.use(express.static(__dirname + '/tests'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
