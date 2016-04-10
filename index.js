531a807ca6da9f10c45e44b2c0f97adff67ab66e
alu0100614220 <alu0100614220@ull.edu.es>
2016-04-10 22:42:30 +0100

    preparando para templates de ejs en el servidor

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ var cool = require('cool-ascii-faces');
 var app = express();
 var path = require('path');
 
-
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public/views'));
 app.use(express.static(__dirname + '/'));
 app.use(express.static(__dirname + '/tests'));
-app.use(express.static(__dirname + '/public/views'));
+require('ejs').delimiter = '$';
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/public/views/index.html'));
-});
-app.get('/cool', function(request, response) {
-  response.send(cool());
+  res.render('index', {title: 'Analizador CSV'});
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port!');
+  console.log('Example app listening on port!' + process.env.PORT);
 });
---
+++ views/main.js
@@ -0,0 +1,15 @@
+main = function() {
+  var original = $('#original').val();
+  if (window.localStorage) localStorage.original = original;
+  var r = calculate(original);
+  var template = fillTable.innerHTML;
+  finaltable.innerHTML = _.template(template, {items: r});
+};
+
+$(document).ready(function () {
+  // If the browser supports localStorage and we have some stored data
+  if (window.localStorage && localStorage.original) {
+    original.value = localStorage.original;
+  }
+  $("button").click(main);
+});
